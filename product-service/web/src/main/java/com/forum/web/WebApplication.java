101d6e88c47d886f652a359a0814ff1bca7c1e5e
Nguyễn Văn Tuyên <v.tuyennv18@vinhomes.vn>
2022-03-11 10:58:32 +0700

    Gán Nhãn

--- product-service/web/src/main/java/com/forum/web/WebApplication.java
+++ product-service/web/src/main/java/com/forum/web/WebApplication.java
@@ -5,15 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.forum")
 @EntityScan(basePackages = "com.forum")
 @ComponentScan(basePackages = "com.forum")
+@RestController
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @GetMapping
+    public String getName() {
+        return "<h2>This is Product Service</h2>";
     }
 
 }
