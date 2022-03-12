c849c8909ea7e8745416b6a3a873fec560580aa4
tuyennv <v.tuyennv18@vinhome.vn>
2022-03-12 11:41:01 +0700

    Reactjs get Data

--- product-service/web/src/main/java/com/forum/web/controller/ProductController.java
+++ product-service/web/src/main/java/com/forum/web/controller/ProductController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("product")
+@CrossOrigin("*")
 public class ProductController {
 
     @Autowired
--- reactjs-webbanhang/src/App.js
+++ reactjs-webbanhang/src/App.js
@@ -1,25 +1,30 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const Product = () => {
+    const fetchProduct = () => {
+        axios.get("http://localhost:8080/product").then(res => {
+            console.log(res);
+        });
+
+    }
+
+    useEffect(() => {
+        fetchProduct()
+    }, []);
+
+    return <h1>Hello</h1>;
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Product />
+        </div>
+    );
 }
 
 export default App;
