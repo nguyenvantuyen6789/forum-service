c2d1cc8dd8d74202cfa1205cb37ab15be8374e50
Admin <nguyenvantuyen6789@gmail.com>
2022-03-22 21:25:56 +0700

    Update Gateway Service

--- gateway-service/src/main/java/com/forum/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/forum/gateway/GatewayServiceApplication.java
@@ -2,12 +2,21 @@ package com.forum.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public String showMenu() {
+		return "<h2><a href='/product'>Product</a></h2>" +
+			   "<h2><a href='/user'>User</a></h2>";
 	}
 
 }
